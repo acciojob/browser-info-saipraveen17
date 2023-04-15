4b5a8aba950f5d379cd7e7c41bfc075a10923316
Praveen Katikireddy <praveenk9417@gmail.com>
2023-04-15 17:04:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const browserInfo = document.getElementById('browser-info');
+
+const browserName = navigator.appName;
+const version = navigator.appVersion;
+
+browserInfo.textContent = `You are using ${browserName} version ${version}.`;
